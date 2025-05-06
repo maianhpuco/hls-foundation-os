6e199134153e8063e6c16ff3b250e3e47de1fc84
Mai Anh Vu <maianhuel@gmail.com>
2025-05-05 20:40:43 -0500

    update

--- viz/visualize_prompt.py
+++ viz/visualize_prompt.py
@@ -4,7 +4,7 @@ import rasterio
 import torch
 import matplotlib.pyplot as plt
 import pandas as pd
-from mmengine.config import Config
+from mmcv.config import Config
  
 # from mmcv import Config
 from mmseg.apis import init_segmentor
